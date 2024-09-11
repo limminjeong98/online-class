137ed68a6d662669fb645ff29a76a05aaf45f4c2
limminjeong98 <limminjeong98@naver.com>
2024-09-11 10:47:46 +0900

    init project

---
+++ src/test/java/com/example/onlineclass/OnlineClassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlineclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineClassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
