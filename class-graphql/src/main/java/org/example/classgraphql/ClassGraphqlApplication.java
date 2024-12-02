7f1d5bf0740e48cf7ffcd37b880e6b4db05ba719
limminjeong98 <limminjeong98@naver.com>
2024-12-02 14:26:22 +0900

    GraphQL 서비스 추가

---
+++ class-graphql/src/main/java/org/example/classgraphql/ClassGraphqlApplication.java
@@ -0,0 +1,13 @@
+package org.example.classgraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassGraphqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClassGraphqlApplication.class, args);
+    }
+
+}
---
+++ class-graphql/src/test/java/org/example/classgraphql/ClassGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.classgraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassGraphqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
