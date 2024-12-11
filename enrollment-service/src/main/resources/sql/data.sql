87fd4e95109effd535ac5b87e49e8a9041e2546d
limminjeong98 <limminjeong98@naver.com>
2024-12-11 09:48:15 +0900

    테스트 데이터 수정

--- enrollment-service/src/main/resources/sql/data.sql
+++ enrollment-service/src/main/resources/sql/data.sql
@@ -1,17 +1,18 @@
 INSERT INTO payments (payment_id, user_id, payment_type, amount, payment_method, payment_date)
-VALUES
-    (100, 100, 'COURSE', 200.00, 'CARD', NOW()),
-    (101, 200, 'COURSE', 150.00, 'CARD', NOW()),
-    (102, 100, 'SUBSCRIPTION', 120.00, 'CARD', NOW()),
-    (103, 200, 'SUBSCRIPTION', 180.00, 'CARD', NOW());
+VALUES (1, 1, 'COURSE', 200.00, 'CARD', NOW()),
+       (2, 1, 'COURSE', 150.00, 'CARD', NOW()),
+       (3, 2, 'COURSE', 200.00, 'CARD', NOW()),
+       (4, 2, 'COURSE', 150.00, 'CARD', NOW()),
+       (5, 1, 'SUBSCRIPTION', 120.00, 'CARD', NOW()),
+       (6, 2, 'SUBSCRIPTION', 180.00, 'CARD', NOW());
 
 INSERT INTO enrollments (enrollment_id, user_id, course_id, payment_id, registration_date)
-VALUES
-    (100, 100, 100, 100, NOW()),
-    (101, 101, 101, 101, NOW());
+VALUES (1, 1, 1, 1, NOW()),
+       (2, 1, 2, 2, NOW()),
+       (3, 2, 1, 3, NOW()),
+       (4, 2, 2, 4, NOW());
 
 
 INSERT INTO subscriptions (subscription_id, user_id, payment_id, start_date, end_date)
-VALUES
-    (100, 100, 102, '2024-05-01 00:00:00', '2024-11-01 00:00:00'),
-    (101, 200, 103, '2024-05-02 00:00:00', '2024-12-01 00:00:00');
+VALUES (1, 1, 5, '2024-05-01 00:00:00', '2024-11-01 00:00:00'),
+       (2, 2, 6, '2024-05-02 00:00:00', '2024-12-01 00:00:00');
--- user-service/src/main/resources/sql/data.sql
+++ user-service/src/main/resources/sql/data.sql
@@ -1,12 +1,12 @@
 INSERT INTO `users` (`id`, `name`, `email`, `password_hash`, `created_at`, `updated_at`)
-VALUES (100, 'John Doe', 'john.doe@example.com', 'hashed_password_1', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP()),
-       (200, 'Jane Smith', 'jane.smith@example.com', 'hashed_password_2', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP()),
-       (300, 'Alice Johnson', 'alice.johnson@example.com', 'hashed_password_3', CURRENT_TIMESTAMP(),
+VALUES (1, 'John Doe', 'john.doe@example.com', 'hashed_password_1', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP()),
+       (2, 'Jane Smith', 'jane.smith@example.com', 'hashed_password_2', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP()),
+       (3, 'Alice Johnson', 'alice.johnson@example.com', 'hashed_password_3', CURRENT_TIMESTAMP(),
         CURRENT_TIMESTAMP()),
-       (400, 'Bob Brown', 'bob.brown@example.com', 'hashed_password_4', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+       (4, 'Bob Brown', 'bob.brown@example.com', 'hashed_password_4', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
 
 INSERT INTO `user_login_histories` (`user_id`, `login_time`, `ip_address`)
-VALUES (100, CURRENT_TIMESTAMP(), '192.168.1.1'),
-       (200, CURRENT_TIMESTAMP(), '192.168.1.2'),
-       (300, CURRENT_TIMESTAMP(), '192.168.1.3'),
-       (400, CURRENT_TIMESTAMP(), '192.168.1.4');
+VALUES (1, CURRENT_TIMESTAMP(), '192.168.1.1'),
+       (2, CURRENT_TIMESTAMP(), '192.168.1.2'),
+       (3, CURRENT_TIMESTAMP(), '192.168.1.3'),
+       (4, CURRENT_TIMESTAMP(), '192.168.1.4');
